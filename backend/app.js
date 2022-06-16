6162d444166e347dd6566ac5cb8be1cae293f397
kamal <kamalkataria32@gmail.com>
2022-06-16 20:11:08 +0530

    backend complete

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,14 @@ const app = express();
 
 const product=require("./routes/productRoute");
 const user=require("./routes/userRoute");
+const order=require("./routes/orderRoute");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require('./middleware/error')
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1",product);
 app.use("/api/v1",user);
-
+app.use("/api/v1",order);
 app.use(errorMiddleware)
 
 
